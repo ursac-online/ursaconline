701f34d3d7371eb73a13fc5d34cd769eac76ea92
ursac-online <114103677+ursac-online@users.noreply.github.com>
2022-11-14 11:47:40 +0800

    changes the image loader in Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -111,7 +111,7 @@ export default function Login() {
     const [selectPosition, setSelectPosition] = useState(true);
     useEffect(() => {
         setTimeout(() => {
-            fetch('http://localhost:3000/')
+            fetch(' https://ursac-online.github.io/ursaconline/')
                 .then(setIsPending(false))
                 .catch(err => {
                     console.log(err.message)
@@ -120,7 +120,7 @@ export default function Login() {
         }, 1510);
         sessionCheck();
         setTimeout(() => {
-            fetch('http://localhost:3000/')
+            fetch(' https://ursac-online.github.io/ursaconline/')
                 .then(setIsLoading(true))
         }, 1500);
     }, [])
